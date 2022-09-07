d52445f9643d53250c4eeec653940a655c4d281c
Fishmen <adam_phoz18@hotmail.com>
2022-09-07 15:19:01 +1000

    updated styles and function for project page

--- src/PtProjectPage.js
+++ src/PtProjectPage.js
@@ -1,18 +1,21 @@
 import React from "react";
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import fitbyfish from './resources/fitbyfish.png';
 import './ptProject.css';
 
 export default function PtProjectPage() {
     return (
         <div className='pt-project'>
-            <div>
+            <div className='head-section'>
                 <h1>Fitness By Fish Personal Training Website</h1>
-                <a href='https://fitnessbyfish.netlify.app/' target='_blank' rel='noreferrer'>To Fitness By Fish Website</a>
+                <a href='https://fitnessbyfish.netlify.app/' target='_blank' rel='noreferrer'>fitnessbyfish.netlify.app</a>
                 
             </div>
             <div>
                 <a  href='https://fitnessbyfish.netlify.app/' target='_blank' rel='noreferrer'><img id='image' src={fitbyfish} title='Website main page' alt='website main page' height='400px' /></a>
+            </div>
+            <div className='project-frameworks'>
+                <p>React - React-Router - Netlify - Tailwind - Firebase - Firestore</p>
             </div>
             <div className='project-description'>
                 <h2>Project Description</h2>
@@ -22,6 +25,9 @@ export default function PtProjectPage() {
                 <p>I also built a exercise tracker section for clients who wanted to track their gym workouts. It allows the client to input their workouts and shows the most recent data to the client.</p>
                 <p>Clients can also search for previous exercises/workouts via date and exercise search parameters.</p>
                 <p>Tailwind was used to style the website.</p>
+            </div>
+            <div>
+                <Link id='link-home' to='/'>Back to home</Link>
             </div>
         </div>
     )
